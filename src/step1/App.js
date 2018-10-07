2a41e05094f8e7e6646f4d3b4499c4acea0e6ff7
gparquet <parquet.geoffrey@gmail.com>
2018-10-07 17:05:04 +0200

    feat: add contact component

--- src/step1/App.js
+++ src/step1/App.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
-
+import Contact from './Contact';
+ 
 class App extends Component{
     render(){
+        const user = {
+        firstName: 'Toto', 
+        lastName: 'Tata', 
+        phoneNumber : '06366565', 
+        email: 'toto.tata@test.fr'};
+        
         return(
             <div>
                 <h1>First Application React</h1>
+                <Contact user={user} />
             </div>    
         );
     }
