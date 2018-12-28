6c2142daf38c7b764d9b84a07abb4ac25168c82e
gparquet <parquet.geoffrey@gmail.com>
2018-12-28 08:44:23 +0100

    refactor: Rename file and comment part of code for live coding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-import HelloComponent from './Hello.component';
+// import HelloComponent from './Hello.component';
 
 class App extends Component{
     render(){
         return(
             <div>
                 <h1>Application React OCN</h1>
-                 <HelloComponent />
+                 {/* <HelloComponent /> */}
             </div>
         )
     }
