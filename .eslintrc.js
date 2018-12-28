6dd37aa6bde3035877e3c679d0754791169ad993
gparquet <parquet.geoffrey@gmail.com>
2018-12-28 22:46:49 +0100

    feat: update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,84 +1,27 @@
-'use strict';
-
-const OFF = 0;
-const ERROR = 2;
-
+const OFF = 0, WARN = 1, ERROR = 2;
 module.exports = {
-  extends: 'fbjs',
-
-  // Stop ESLint from looking for a configuration file in parent folders
-  'root': true,
-
-  plugins: [
-    'jest',
-    'no-for-of-loops',
-    'react',
-    'react-internal',
-  ],
-
-  // We're stricter than the default config, mostly. We'll override a few rules
-  // and then enable some React specific ones.
-  rules: {
-    'accessor-pairs': OFF,
-    'brace-style': [ERROR, '1tbs'],
-    'comma-dangle': [ERROR, 'always-multiline'],
-    'consistent-return': OFF,
-    'dot-location': [ERROR, 'property'],
-    'dot-notation': ERROR,
-    'eol-last': ERROR,
-    'eqeqeq': [ERROR, 'allow-null'],
-    'indent': OFF,
-    'jsx-quotes': [ERROR, 'prefer-double'],
-    'keyword-spacing': [ERROR, {after: true, before: true}],
-    'no-bitwise': OFF,
-    'no-inner-declarations': [ERROR, 'functions'],
-    'no-multi-spaces': ERROR,
-    'no-restricted-syntax': [ERROR, 'WithStatement'],
-    'no-shadow': ERROR,
-    'no-unused-expressions': ERROR,
-    'no-unused-vars': [ERROR, {args: 'none'}],
-    'no-useless-concat': OFF,
-    'quotes': [ERROR, 'single', {avoidEscape: true, allowTemplateLiterals: true }],
-    'space-before-blocks': ERROR,
-    'space-before-function-paren': OFF,
-
-    // React & JSX
-    // Our transforms set this automatically
-    'react/jsx-boolean-value': [ERROR, 'always'],
-    'react/jsx-no-undef': ERROR,
-    // We don't care to do this
-    'react/jsx-sort-prop-types': OFF,
-    'react/jsx-space-before-closing': ERROR,
-    'react/jsx-uses-react': ERROR,
-    'react/no-is-mounted': OFF,
-    // This isn't useful in our test code
-    'react/react-in-jsx-scope': ERROR,
-    'react/self-closing-comp': ERROR,
-    // We don't care to do this
-    'react/jsx-wrap-multilines': [ERROR, {declaration: false, assignment: false}],
-
-    // Prevent for...of loops because they require a Symbol polyfill.
-    // You can disable this rule for code that isn't shipped (e.g. build scripts and tests).
-    'no-for-of-loops/no-for-of-loops': ERROR,
-
-    // CUSTOM RULES
-    // the second argument of warning/invariant should be a literal string
-    'react-internal/no-primitive-constructors': ERROR,
-    'react-internal/warning-and-invariant-args': ERROR,
+  "extends": ["airbnb"],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 9,
+    "sourceType": "module",
+    "allowImportExportEverywhere": false,
+    "codeFrame": false,
+    "ecmaFeatures": {
+      globalReturn: true,
+      impliedStrict: true,
+      jsx: true,
+      arrowFunction: true,
+     },
   },
-
-  overrides: [
-    {
-      files: ['**/__tests__/*.js'],
-      rules: {
-        'jest/no-focused-tests': ERROR,
-      }
-    }
-  ],
-
-  globals: {
-    spyOnDev: true,
-    spyOnDevAndProd: true,
-    spyOnProd: true,
+  "plugins": ["react"],
+  "env": {
+    "es6": true,
+    "browser": true
   },
+ "rules":{
+    "react/jsx-filename-extension": [WARN, { "extensions": [".js", ".jsx"] }],
+    "react/jsx-indent": [OFF],
+    "linebreak-style": [ERROR, "windows"]
+  }
 };
--- webpack.common.js
+++ webpack.common.js
@@ -3,32 +3,37 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: './src/main.js'
-    },
-    devServer: {
-		port: 1988
-	},
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'TweeterFake',
-            template: './index.html',
-            filename: './index.html'
-        })
+  entry: {
+    app: './src/main.js',
+  },
+  devServer: {
+    port: 1988,
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'TweeterFake',
+      template: './index.html',
+      filename: './index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { presets: ['es2015', 'react'] },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
     ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                query: { presets: ['es2015', 'react'] }
-            }
-        ]
-    }
+  },
 };
